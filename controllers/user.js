a30e1aa72bd460304b9e46a2f5411cd3e452e7ef
abhisheksoni123 <abhisheksoni@abhisheks-MBP-2.lan>
2026-01-06 13:31:35 +0530

    changes in response after signup and login

--- controllers/user.js
+++ controllers/user.js
@@ -2,31 +2,33 @@ const User = require("../models/user");
 
 async function handleCreateUser(req, res) {
   const { fullName, email, password } = req.body;
-  await User.create({
+  const user = await User.create({
     fullName,
     email,
     password,
   });
-  res.status(201).json({
-    message: "User registered successfully",
+  res.status(200).json({
+    user: {
+      _id: user._id,
+      email: user.email,
+      role: user.role,
+    },
   });
 }
 
 async function handleLoginUser(req, res) {
   const { email, password } = req.body;
+
   try {
-    console.log("email is", email);
-    console.log("password is", password);
+    const user = await User.matchPassword(email, password);
 
-    const isValid = await User.matchPassword(email, password);
-
-    if (!isValid) {
-      return res.status(401).json({ error: "Invalid email or password" });
-    }
-
-    // 3️⃣ Success
+    // ✅ Success response
     res.status(200).json({
-      message: "Login successful",
+      user: {
+        _id: user._id,
+        email: user.email,
+        role: user.role,
+      },
     });
   } catch (err) {
     res.status(401).json({
